13ab7c4c8127bf65c1261dc5e6e8ea4e4104ee19
Eric Kivel <ericvkivel1@gmail.com>
2020-11-06 21:22:26 -0300

    Controllers

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
 
 import MovementsController from './controllers/MovementsController';
+import CategoriesController from './controllers/CategoriesController';
+import BalanceByNameController from './controllers/BalanceByNameController';
 
 const movementsController = new MovementsController();
+const categoriesController = new CategoriesController();
+const balanceByName = new BalanceByNameController();
 
 const routes = Router();
 
 routes.post('/movements', movementsController.create);
+routes.get('/movements', movementsController.index);
+
+routes.post('/categories', categoriesController.create);
+routes.get('/categories', categoriesController.index);
+
+routes.get('/balance/name', balanceByName.index);
 
 export default routes;
