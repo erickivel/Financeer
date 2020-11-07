fd3876ce118b9370d382518e91c73f24613fa3c5
Eric Kivel <ericvkivel1@gmail.com>
2020-11-06 21:21:03 -0300

    Create AppErrors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
-
-import express from 'express';
 import cors from 'cors';
 
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
+import AppError from './errors/AppError';
 
 import './database/connection';
 
@@ -13,6 +15,22 @@ app.use(cors());
 
 app.use(express.json());
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
