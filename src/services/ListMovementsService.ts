0da5f8378e1ccbbfb033dc1cfc1d68f07bad66a7
Eric Kivel <ericvkivel1@gmail.com>
2020-11-06 21:22:12 -0300

    Feature: list movements

---
+++ src/services/ListMovementsService.ts
@@ -0,0 +1,27 @@
+import { getCustomRepository } from 'typeorm';
+
+import Category from '../models/Category';
+import MovementsRepository from '../repositories/MovementsRepository';
+
+interface MovementWithCategory {
+  product_name: string;
+  category_id: string;
+  movement_date: Date;
+  financial_institution: string;
+  value_applied: number;
+  movement_type: 'application' | 'redemption';
+  amount: number;
+  category: Category | undefined;
+}
+
+class ListMovementsService {
+  public async execute(): Promise<MovementWithCategory[]> {
+    const movementsRepository = getCustomRepository(MovementsRepository);
+
+    const movementsWithCategory = await movementsRepository.findWithCategory();
+
+    return movementsWithCategory;
+  }
+}
+
+export default ListMovementsService;
