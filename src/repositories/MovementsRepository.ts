c9732a8d049db09416e74b9b1dd7962b46a21686
Eric Kivel <ericvkivel1@gmail.com>
2020-11-06 21:26:57 -0300

    Feature: list movements

--- src/repositories/MovementsRepository.ts
+++ src/repositories/MovementsRepository.ts
@@ -1,8 +1,40 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, getRepository } from 'typeorm';
 
 import Movement from '../models/Movement';
+import Category from '../models/Category';
+
+interface MovementWithCategory {
+  product_name: string;
+  category_id: string;
+  movement_date: Date;
+  financial_institution: string;
+  value_applied: number;
+  movement_type: 'application' | 'redemption';
+  amount: number;
+  category: Category | undefined;
+}
 
 @EntityRepository(Movement)
-class MovementsRepository extends Repository<Movement> {}
+class MovementsRepository extends Repository<Movement> {
+  public async findWithCategory(): Promise<MovementWithCategory[]> {
+    const movementsRepository = getRepository(Movement);
+    const categoriesRepository = getRepository(Category);
+
+    const movements = await movementsRepository.find();
+    const categories = await categoriesRepository.find();
+
+    const movementsWithCategory = movements.map(movement => {
+      return {
+        ...movement,
+        value_applied: Number(movement.value_applied),
+        category: categories.find(
+          category => category.id === movement.category_id,
+        ),
+      };
+    });
+
+    return movementsWithCategory;
+  }
+}
 
 export default MovementsRepository;
