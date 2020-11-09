b41ae1a6ddccf079db9d5bb19d7959f5b9080068
Eric Kivel <ericvkivel1@gmail.com>
2020-11-08 21:06:02 -0300

    Fix: list balances by name

--- src/repositories/MovementsRepository.ts
+++ src/repositories/MovementsRepository.ts
@@ -14,6 +14,12 @@ interface MovementWithCategory {
   category: Category | undefined;
 }
 
+interface BalanceByName {
+  product_name: string;
+  total_value_invested: number;
+  total_amount: number;
+}
+
 @EntityRepository(Movement)
 class MovementsRepository extends Repository<Movement> {
   public async findWithCategory(): Promise<MovementWithCategory[]> {
@@ -34,6 +40,68 @@ class MovementsRepository extends Repository<Movement> {
     });
 
     return movementsWithCategory;
+  }
+
+  public async getAllBallancesByProductName(): Promise<BalanceByName[]> {
+    const movementsRepository = getRepository(Movement);
+
+    const movements = await movementsRepository.find();
+
+    const movementsNames = movements.map(movement => movement.product_name);
+
+    const movementsWithoutDuplicateNames = movementsNames.filter(
+      (product_name, index) => movementsNames.indexOf(product_name) === index,
+    );
+
+    const movementsBalancesByName = movementsWithoutDuplicateNames
+      .map(product_name => {
+        const movementsFilteredByName = movements.filter(
+          movement => movement.product_name === product_name,
+        );
+
+        const total_value_invested = movementsFilteredByName.reduce(
+          (prevValueApplied, currMovement) => {
+            if (currMovement.movement_type === 'redemption') {
+              return (
+                Math.round(
+                  (prevValueApplied - Number(currMovement.movement_value)) *
+                    100,
+                ) / 100
+              );
+            }
+            return (
+              Math.round(
+                (prevValueApplied + Number(currMovement.movement_value)) * 100,
+              ) / 100
+            );
+          },
+          0,
+        );
+
+        const total_amount = movementsFilteredByName.reduce(
+          (prevAmount, currMovement) => {
+            if (currMovement.movement_type === 'redemption') {
+              return (
+                Math.round((prevAmount - Number(currMovement.amount)) * 100) /
+                100
+              );
+            }
+            return (
+              Math.round((prevAmount + Number(currMovement.amount)) * 100) / 100
+            );
+          },
+          0,
+        );
+
+        return {
+          product_name,
+          total_value_invested,
+          total_amount,
+        };
+      })
+      .filter(balance => balance.total_amount > 0);
+
+    return movementsBalancesByName;
   }
 }
 
--- src/services/ListBalancesByMovementNamesService.ts
+++ src/services/ListBalancesByMovementNamesService.ts
@@ -1,76 +1,19 @@
-import { getRepository } from 'typeorm';
+import { getCustomRepository } from 'typeorm';
 
-import Movement from '../models/Movement';
+import MovementsRepository from '../repositories/MovementsRepository';
 
-interface MovementBalanceByName {
+interface BalanceByName {
   product_name: string;
   total_value_invested: number;
   total_amount: number;
 }
-
 class ListBalanceByMovementsNameService {
-  public async execute(): Promise<MovementBalanceByName[]> {
-    const movementsRepository = getRepository(Movement);
+  public async execute(): Promise<BalanceByName[]> {
+    const movementsRepository = getCustomRepository(MovementsRepository);
 
-    const movementsWithCategory = await movementsRepository.find();
+    const balancesByName = await movementsRepository.getAllBallancesByProductName();
 
-    const movementsNames = movementsWithCategory.map(
-      movement => movement.product_name,
-    );
-
-    const movementsWithoutDuplicateNames = movementsNames.filter(
-      (product_name, index) => movementsNames.indexOf(product_name) === index,
-    );
-
-    const movementsBalancesByName = movementsWithoutDuplicateNames.map(
-      product_name => {
-        const movementsFilteredByName = movementsWithCategory.filter(
-          movement => movement.product_name === product_name,
-        );
-
-        const total_value_invested = movementsFilteredByName.reduce(
-          (prevValueApplied, currMovement) => {
-            if (currMovement.movement_type === 'redemption') {
-              return (
-                Math.round(
-                  (prevValueApplied - Number(currMovement.movement_value)) *
-                    100,
-                ) / 100
-              );
-            }
-            return (
-              Math.round(
-                (prevValueApplied + Number(currMovement.movement_value)) * 100,
-              ) / 100
-            );
-          },
-          0,
-        );
-
-        const total_amount = movementsFilteredByName.reduce(
-          (prevAmount, currMovement) => {
-            if (currMovement.movement_type === 'redemption') {
-              return (
-                Math.round((prevAmount - Number(currMovement.amount)) * 100) /
-                100
-              );
-            }
-            return (
-              Math.round((prevAmount + Number(currMovement.amount)) * 100) / 100
-            );
-          },
-          0,
-        );
-
-        return {
-          product_name,
-          total_value_invested,
-          total_amount,
-        };
-      },
-    );
-
-    return movementsBalancesByName;
+    return balancesByName;
   }
 }
 
