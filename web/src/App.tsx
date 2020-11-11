fc88703f95508519a4e3feecd511539b3ae8fbdc
Eric Kivel <ericvkivel1@gmail.com>
2020-11-11 19:06:12 -0300

    React Setup

---
+++ web/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
