df3fb327c65ccd1fa74b2ea738b5ecdd7da3a7a6
Eric Kivel <ericvkivel1@gmail.com>
2020-11-13 19:01:49 -0300

    Extract page

---
+++ web/src/pages/Extract/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+
+import { Container } from './styles';
+
+const Extract: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Container>
+        <h1>Extrato</h1>
+      </Container>
+    </>
+  );
+};
+
+export default Extract;
