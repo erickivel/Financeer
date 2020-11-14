99e1a460a8a6d782ac9218e12880eab83c638ff8
Eric Kivel <ericvkivel1@gmail.com>
2020-11-14 16:24:42 -0300

    Extract

--- web/src/pages/Extract/index.tsx
+++ web/src/pages/Extract/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from '../../components/Header';
 
-import { Container } from './styles';
+import { Container, TableContainer } from './styles';
 
 const Extract: React.FC = () => {
   return (
@@ -10,6 +10,42 @@ const Extract: React.FC = () => {
       <Header />
       <Container>
         <h1>Extrato</h1>
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Quantidade</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Tipo</th>
+                <th>Instituição Financeira</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>ITSA4</td>
+                <td>12432</td>
+                <td className="application">R$ 12423,12</td>
+                <td>Ações</td>
+                <td className="application">Aplicação</td>
+                <td>Rico</td>
+                <td>14/11/2020</td>
+              </tr>
+              <tr>
+                <td>ITSA4</td>
+                <td>12432</td>
+                <td className="redemption">R$ 1241,12</td>
+                <td>Ações</td>
+                <td className="redemption">Resgate</td>
+                <td>Rico</td>
+                <td>14/11/2020</td>
+              </tr>
+            </tbody>
+          </table>
+        </TableContainer>
       </Container>
     </>
   );
--- web/src/pages/Extract/styles.ts
+++ web/src/pages/Extract/styles.ts
@@ -1,3 +1,61 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  max-width: 1120px;
+  width: 100%;
+  margin: 0 auto;
+
+  h1 {
+    margin-top: 76px;
+    font-family: 'Poppins';
+    font-weight: 500;
+    font-size: 36px;
+    text-align: center;
+    color: #464141;
+  }
+`;
+
+export const TableContainer = styled.div`
+  margin-top: 64px;
+
+  table {
+    width: 100%;
+    border-spacing: 0 8px;
+
+    th {
+      color: #636363;
+      font-weight: normal;
+      font-family: 'Poppins';
+      padding: 20px 32px;
+      text-align: left;
+      font-size: 16px;
+      line-height: 24px;
+    }
+
+    td {
+      padding: 20px 32px;
+      border: 0;
+      background: #f4ede8;
+      font-family: 'Poppins';
+      font-size: 16px;
+      font-weight: normal;
+      color: #464141;
+    }
+
+    td.application {
+      color: #12a454;
+    }
+
+    td.redemption {
+      color: #e83f5b;
+    }
+
+    td:first-child {
+      border-radius: 8px 0 0 8px;
+    }
+
+    td:last-child {
+      border-radius: 0 8px 8px 0;
+    }
+  }
+`;
