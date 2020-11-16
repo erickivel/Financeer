4415b1bc66e4177b641d2560988bbd997bb7b18b
Eric Kivel <ericvkivel1@gmail.com>
2020-11-16 16:12:11 -0300

    Fix: db connection

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   type: "mysql",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "docker",
+  database: process.env.DB_DATABASE || "app-db",
   extra: { insecureAuth: true },
   entities: [
     "./src/models/*.ts"
